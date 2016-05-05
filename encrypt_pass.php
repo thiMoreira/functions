6c26fb93b6e2753c8a257ee42ff964f26471a161
thiago <tmsmoreira@yahoo.com.br>
2016-05-05 18:43:27 -0300

    Função para criptografar senha

--- encrypt_pass.php
+++ encrypt_pass.php
@@ -12,7 +12,7 @@
         $string  = strrev( $password );
         $string  = str_repeat( md5( $password ) , 2 );
         $string  = md5( $string );
-        $string .= md5( "k49fdk4030d" );
+        $string .= md5( "43kdek#@dsdk74c85c48d5!4fd8#fafdakdj#49" );
         $string  = md5( $string );
         
         return $string;
